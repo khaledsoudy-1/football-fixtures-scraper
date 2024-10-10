72e571ccc397bd28b09bfec275755b8faea69aab
Khaled Soudy <khaledsoudy2012@gmail.com>
2024-10-10 08:19:06 +0300

    Improve fetching function with error handling and custom headers

--- main.py
+++ main.py
@@ -5,12 +5,24 @@ page_url = "https://www.yallakora.com/match-center/?date=10/10/2024"
 
 
 def fetch_page_content(url):
-    page = requests.get(url)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
+    }
     
-    if page.status_code == 200:
-        print(page.content)
-    else:
-        print("An error occurred")
+    try:
+        page = requests.get(url, headers=headers, timeout=5)
+        page.raise_for_status()  # Raises an error for non-successful requests
+        return page.content  # Return HTML page content
+    
+    except requests.exceptions.HTTPError as http_error:
+        print(f"HTTP error occurred: {http_error}")
+    except requests.exceptions.Timeout as timeout_error:
+        print(f"Timeout error occurred: {timeout_error}")
+    except requests.exceptions.ConnectionError as connect_error:
+        print(f"Connection error occurred: {connect_error}")
+    except requests.exceptions.RequestException as error:
+        print(f"An error occurred: {error}")
+    return None
 
 
 fetch_page_content(page_url)
